7c315e2aa7308ab3d5a47774905c42f5779642dc
Deepak <dbehera647@gmail.com>
2022-12-15 12:17:17 +0530

    Rendering pages for  Sign In & sign UP

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,17 @@ module.exports.profile=function(req,res){
           title:"Users Profile"
      })
 }
+
+//render the sign up page
+module.exports.signUp=function(req,res){
+     res.render('user_sign_up',{
+          title:"Codeial / Sign Up"
+     })
+}
+
+//render the sign in page
+module.exports.signIn=function(req,res){
+     res.render('user_sign_in',{
+          title:"Codeial / Sign In"
+     })
+}
