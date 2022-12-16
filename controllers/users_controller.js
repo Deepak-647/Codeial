d07c74998c584fe33a80e4dfef1b9db45868978a
Deepak <dbehera647@gmail.com>
2022-12-16 12:32:21 +0530

    User SignUp Authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,29 +1,51 @@
-module.exports.profile=function(req,res){
-     res.render('users',{
-          title:"Users Profile"
-     })
-}
+const User =require('../models/user')
+
+module.exports.profile = function (req, res) {
+  res.render("users", {
+    title: "Users Profile",
+  });
+};
 
 //render the sign up page
-module.exports.signUp=function(req,res){
-     res.render('user_sign_up',{
-          title:"Codeial / Sign Up"
-     })
-}
+module.exports.signUp = function (req, res) {
+  res.render("user_sign_up", {
+    title: "Codeial / Sign Up",
+  });
+};
+
 
 //render the sign in page
-module.exports.signIn=function(req,res){
-     res.render('user_sign_in',{
-          title:"Codeial / Sign In"
-     })
-}
+module.exports.signIn = function (req, res) {
+  res.render("user_sign_in", {
+    title: "Codeial / Sign In",
+  });
+};
 
-//get the sign up data 
-module.exports.create=function(req,res){
-     //TODO later
-}
+//get the sign up data
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    return redirect("back");
+  }
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("error in finding user signing up");
+      return;
+    }
+    if (!user) {
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log("error in creating user while signing up");
+          return;
+        }
+        return res.redirect("/users/sign-in");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
 
 //get the sign in data
-module.exports.createSession=function(req,res){
-     //TODO later
-}
+module.exports.createSession = function (req, res) {
+  //TODO later
+};
